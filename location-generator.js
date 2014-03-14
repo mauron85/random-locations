9c7976eca142f4d287397142189958af1db065d6
Marian Hello <marian.hello@gmail.com>
2014-03-14 20:13:21 +0100

    making module UMD

--- location-generator.js
+++ location-generator.js
@@ -1,33 +1,50 @@
 //http://gis.stackexchange.com/questions/25877/how-to-generate-random-locations-nearby-my-location
 
-function Random () {
-}
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.Config = factory();
+  }
+}(this, function () {
 
-Random.prototype.nextvar = function () {
-	return Math.random();
-}
+    function Random () {
+    }
 
-function LocationGenerator () {
-    this.random = new Random();
-}
+    Random.prototype.nextvar = function () {
+    	return Math.random();
+    }
 
-LocationGenerator.prototype.nextLocation = function (x0, y0, radius) {
+    function LocationGenerator () {
+        this.random = new Random();
+    }
 
-    // Convert radius from meters to degrees
-    var radiusInDegrees = radius / 111000;
+    LocationGenerator.prototype.nextLocation = function (x0, y0, radius) {
 
-    var u = this.random.nextvar();
-    var v = this.random.nextvar();
-    var w = radiusInDegrees * Math.sqrt(u);
-    var t = 2 * Math.PI * v;
-    var x = w * Math.cos(t);
-    var y = w * Math.sin(t);
+        // Convert radius from meters to degrees
+        var radiusInDegrees = radius / 111000;
 
-    // Adjust the x-coordinate for the shrinking of the east-west distances
-    var new_x = x / Math.cos(y0);
+        var u = this.random.nextvar();
+        var v = this.random.nextvar();
+        var w = radiusInDegrees * Math.sqrt(u);
+        var t = 2 * Math.PI * v;
+        var x = w * Math.cos(t);
+        var y = w * Math.sin(t);
 
-    var foundLongitude = new_x + x0;
-    var foundLatitude = y + y0;
-    return [foundLatitude, foundLongitude];
-}
+        // Adjust the x-coordinate for the shrinking of the east-west distances
+        var new_x = x / Math.cos(y0);
 
+        var foundLongitude = new_x + x0;
+        var foundLatitude = y + y0;
+        return [foundLatitude, foundLongitude];
+    }
+
+    return LocationGenerator;
+}));
